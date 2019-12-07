0da24144c5f8f971965a20a88d8cbb918aa735a3
wdunn <williamdunn195@gmail.com>
2019-12-06 21:28:22 -0500

    Updates

---
+++ precache-manifest.21ebd85db379a9403d90a635a6e55cb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d04094bc3cccc0b7743e8ae56706d65f",
+    "url": "/github-jobs-search/index.html"
+  },
+  {
+    "revision": "2fb927b6548095eef6e4",
+    "url": "/github-jobs-search/static/css/main.197c0565.chunk.css"
+  },
+  {
+    "revision": "803c9ba585028fd97a95",
+    "url": "/github-jobs-search/static/js/2.bf9c3c80.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/github-jobs-search/static/js/2.bf9c3c80.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2fb927b6548095eef6e4",
+    "url": "/github-jobs-search/static/js/main.79e542d5.chunk.js"
+  },
+  {
+    "revision": "ace62e2527466ef4b895",
+    "url": "/github-jobs-search/static/js/runtime-main.1fdca786.js"
+  },
+  {
+    "revision": "cefc20232703e5e3c24efd5f50d75e26",
+    "url": "/github-jobs-search/static/media/GitHub_Logo.cefc2023.png"
+  }
+]);
